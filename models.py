1620687865f69065737331d0ed2e497e9709943d
unknown <da.arias331@gmail.com>
2024-04-15 15:35:41 +1000

    model and model config created

--- models.py
+++ models.py
@@ -22,3 +22,16 @@ class CIFAR10Model(nn.Module):
     """
     Hint: It is going to be very similar to SimpleModel
     """
+
+    def __init__(self, backbone: nn.Module, adapter: nn.Module, head: nn.Module) -> None:
+        super().__init__()
+        self.backbone = backbone
+        self.adapter = adapter
+        self.head = head
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = x.repeat(1, 3, 1, 1)
+        x = self.backbone(x)
+        x = self.adapter(x)
+        x = self.head(x)
+        return x
